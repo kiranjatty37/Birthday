e3296769e7e496a9001880551809f04c5d72bebb
kiranjatty37 <kj272@scarletmail.rutgers.edu>
2017-07-13 23:05:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ var canvas = document.getElementById("canvas"),
 		ctx = canvas.getContext("2d"),
     keyword = "HAPPY BIRTHDAY SUNNY",
 		imageData,
-		density = 2,
+		density = 1,
 		mouse = {},
 		hovered = false,
 		colors = ["236, 252, 17", "233, 150, 15","10, 230, 255","254, 230, 211","255, 0, 0","20, 255, 10"],
 		minDist = 30,
-		bounceFactor = 1;
+		bounceFactor = 0.5;
 var img = document.getElementById('source');
 var image=[];
 for(var i=0; i<6; i++)
